42de184b2fa73b1915f7d2a71499ad4b16c6d34e
Reetesh Kumar <rajreetesh7@gmail.com>
2023-09-03 20:52:41 +0530

    added client project

---
+++ client/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+
+const font = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Discord",
+  description: "A app built for blazing fast chat experience.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
